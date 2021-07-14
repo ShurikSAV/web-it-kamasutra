1ea0a9372054dbd6f4f223830bdf1ca732985359
ShurikS <Shurik.SAV@Gmail.com>
2021-07-14 23:04:16 +0700

    list

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Simple HTML
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
     </div>
   );
 }
