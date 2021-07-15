44b15ac27239379e46bf65fc36dc763c77ec85fb
ShurikS <Shurik.SAV@Gmail.com>
2021-07-15 22:36:47 +0700

    del

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import Technologies from './technologies'
-import Header from './header'
-import Footer from './footer'
 
 import './App.css'
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <Technologies />
-      <Footer />
     </div>
   )
 }
