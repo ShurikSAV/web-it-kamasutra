c7d9487f52ad77a3d7ae26b8b549d7ddba38e20b
ShurikS <Shurik.SAV@Gmail.com>
2021-07-15 22:32:17 +0700

    Footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Technologies from './technologies'
 import Header from './header'
-import './App.css';
+import Footer from './footer'
+
+import './App.css'
 
 const App = () => {
   return (
     <div>
       <Header />
       <Technologies />
+      <Footer />
     </div>
-  );
+  )
 }
 
 export default App
