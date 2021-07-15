ff1999ea584cbfb101b0dfa8a862815fc8a06e9c
ShurikS <Shurik.SAV@Gmail.com>
2021-07-15 22:58:12 +0700

    nav

--- src/App.js
+++ src/App.js
@@ -9,8 +9,11 @@ const App = () => {
         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4MuXyd8KEmHlFuKFbMftU-rcm3zcORkaQCw&usqp=CAU'></img>
       </header>
       <nav className='nav'>
-        <div>Profile</div>
-        <div>Message</div>
+        <div><a>Profile</a></div>
+        <div><a>Messages</a></div>
+        <div><a>News</a></div>
+        <div><a>Music</a></div>
+        <div><a>Settings</a></div>
       </nav>
       <div className='content'>Main content</div>
     </div>
