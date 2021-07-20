43db2522f15e0d65bb4b55223a0832488d4448d4
Savelyev <Shurik.SAV@Gmail.com>
2021-07-20 16:16:51 +0700

    module.css

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import './profile'
+import s from './header.module.css'
 
 const Header = () => {
   return (
-      <header className='header'>
+      <header className={s.header}>
         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4MuXyd8KEmHlFuKFbMftU-rcm3zcORkaQCw&usqp=CAU'></img>
       </header>
   );
-  
+   
 }
 
 export default Header;
--- src/components/nav-bar.jsx
+++ src/components/nav-bar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import './nav-bar.css'
+import s from './nav-bar.module.css'
 
 const NavBar = () => {
   return (
-      <nav className='nav'>
-        <div className='item'><a>Profile</a></div>
-        <div className='item'><a>Messages</a></div>
-        <div className='item'><a>News</a></div>
-        <div className='item'><a>Music</a></div>
-        <div className='item'><a>Settings</a></div>
+      <nav className={s.nav}>
+        <div className={s.item}><a>Profile</a></div>
+        <div className={s.item}><a>Messages</a></div>
+        <div className={s.item}><a>News</a></div>
+        <div className={s.item}><a>Music</a></div>
+        <div className={s.item}><a>Settings</a></div>
       </nav>
   );
   
