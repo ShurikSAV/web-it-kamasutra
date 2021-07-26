a86cddba11e4d588c3989f639d747b0d62a1997d
ShurikS <Shurik.SAV@Gmail.com>
2021-07-26 22:58:26 +0700

    Post

--- src/components/profile/my-posts/my-posts.jsx
+++ src/components/profile/my-posts/my-posts.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import s from './my-posts.module.css'
+//import s from './my-posts.module.css'
+import Post from "./post/post";
 
 const MyPosts = () => {
-  return (
-      <div>
-        My posts
+    return (
         <div>
-          new post
+            My posts
+            <div>
+                <textarea></textarea>
+                <button>Add post</button>
+            </div>
+           <Post/>
+           <Post/>
+           <Post/>
+           <Post/>
         </div>
-        <div className={s.posts}>
-          <div className={s.item}>
-            Post1
-          </div>
-          <div className={s.item}>
-            Post2
-          </div>
-        </div>
-      </div>
-  );
+    );
 }
 
 export default MyPosts;
--- src/components/profile/my-posts/post/post.jsx
+++ src/components/profile/my-posts/post/post.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import s from './my-posts.module.css'
+import s from './post.module.css'
 
 const Post = () => {
-  return (
+    return (
         <div className={s.posts}>
-          <div className={s.item}>
-            Post1
-          </div>
-          <div className={s.item}>
-            Post2
-          </div>
+            <div className={s.item}>
+                <img
+                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw9cnEvZ98kadKb7ZIhW20sXOTNVlEsH1q4Q&usqp=CAU'/>
+                Post1
+                <div>
+                    <span>like</span>
+                </div>
+            </div>
         </div>
-  );
+    );
 }
 
 export default Post;
