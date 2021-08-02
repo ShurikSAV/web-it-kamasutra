4b468ef52f6b0b08d69dc0db61051b9edc5ada22
ShurikS <Shurik.SAV@Gmail.com>
2021-08-03 00:01:53 +0700

    24. Уроки React JS (UI - BLL, выносим данные в сторону)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -19,21 +19,29 @@ const Message = (props) => {
 }
 
 const Dialogs = () => {
+    let dialogsData = [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Victor'},
+        {id: 6, name: 'Valera'},
+        {id: 7, name: 'SAV'}
+    ]
+    let messageData = [
+        {id: 1, name: 'Hi'},
+        {id: 2, name: 'How is your ..'},
+        {id: 3, name: 'ok'},
+        {id: 4, name: 'ok'}
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-
-                <DialogItem name='Dimych' id='1'/>
-                <DialogItem name='Andrey' id='2'/>
-                <DialogItem name='Sveta' id='3'/>
-                <DialogItem name='Sasha' id='4'/>
-                <DialogItem name='Victor' id='5'/>
-                <DialogItem name='Valera' id='6'/>
+                {dialogsData.map(m => <DialogItem name={m.name} id={m.id}/>)}
             </div>
             <div className={s.messages}>
-                <Message message='Hi' />
-                <Message message='How is your ..' />
-                <Message message='ok' />
+                {messageData.map(m => <Message message={m.name}/>)}
             </div>
         </div>
     )
